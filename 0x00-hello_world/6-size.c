b56b6f909641af87462ebfad6fe80e3cbadf147e
Joshua111512 <joshuanodiba14724@gmail.com>
2022-10-17 13:41:57 -0700

    my first c code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,8 @@ int main(void)
 
 	printf("size ofa char: %zu byte(s)\n", (unsigned long)sizeof(a));
 	printf("size of an int: %zu byte(s)\n", (unsigned long)sizeof(b));
-	printf("size of a long int: %zu byte(s)\n", (unsigned long)sizeof(c));
-	printf("size of a long long int: %zu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("size of a float: %zu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
