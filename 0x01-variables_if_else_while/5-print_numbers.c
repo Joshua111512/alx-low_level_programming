fc47cc8317c97a9a9ea768c9a89360a99b49979a
Joshua111512 <joshuanodiba14724@gmail.com>
2022-10-20 10:53:37 -0700

    if else while commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all last digit stored in the variable n by assigning
+ * a random number to variable n each time it is executed
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n)
+	}
+	printf("\n");
+	return (0);
+}
