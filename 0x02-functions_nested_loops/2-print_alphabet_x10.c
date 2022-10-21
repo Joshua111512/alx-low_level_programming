ac60a9b24f80a61a2be82d0f886c73119f3d7791
Joshua111512 <joshuanodiba14724@gmail.com>
2022-10-21 04:19:31 -0700

    lowercase 10x more

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints 10 times the alphabet, in lowercase
- *
+ * print_alphabet_x10  - prints 10 times the alphabet, in lowercase
  */
 void print_alphabet_x10(void)
 {
@@ -15,4 +14,3 @@ void print_alphabet_x10(void)
 		_putchar(letter);
 	_putchar('\n');
 	}
-}
